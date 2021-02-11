63970bc7d3f90e64088f53d6a0a79b8e9c8c15a1
djib2011 <thanostagaris@gmail.com>
2021-02-11 18:18:36 +0200

    Cleanup sequential model builder and add 'get' function

--- networks/__init__.py
+++ networks/__init__.py
@@ -1,1 +1,21 @@
-from networks import sequential, convolutional
+import tensorflow as tf
+from networks import sequential
+
+
+def get(**params) -> tf.keras.models.Model:
+    """
+    Build and compile the tf.keras model with the desired properties.
+
+    :param params: Parameters describing how to build the model
+    :return: the desired tf.keras model
+    """
+
+    default = {'input_seq_len': 18, 'output_seq_len': 6, 'layer_size': 64, 'encoder_type': 'bi', 'encoder_layers': 2,
+               'decoder_type': 'lstm', 'decoder_layers': 1, 'attention_type': 'mul', 'attention_scale': False,
+               'attention_dropout': 0., 'attention_causal': False}
+
+    default.update(params)
+
+    if default['encoder_type'] in ('uni', 'bi') and default['decoder_type'] in ('uni', 'bi'):
+
+        return sequential.build_model(default)
--- networks/sequential.py
+++ networks/sequential.py
@@ -1,121 +1,109 @@
 import tensorflow as tf
 
 
-def enc2bi_dec1bi(hparams: dict) -> tf.keras.models.Model:
+def build_model(hparams: dict) -> tf.keras.models.Model:
     """
-    Factory for creating a forecaster
+    Factory for creating a (possibly attention-based) encoder-decoder forecaster.
 
-    :param hparams: a dictionary containing some hyperparameters that determine how the forecaster will be built
-    :return: a tf.keras model
+    hparams should include:
+
+        - 'input_seq_length': size of the input sequence
+        - 'output_seq_length': size of the output sequence
+        - 'encoder_type': type of the encoder layers ('bi' or 'uni')
+        - 'num_encoder_layers': number of layers in the encoder
+        - 'decoder_type': type of the decoder layers ('bi' or 'uni')
+        - 'num_decoder_layers': number of layers in the decoder
+        - 'attention_type': what type of attention to use ('add', 'mul', 'self' or None)
+        - 'attention_scale': choose whether or not to scale the attention scores or not
+        - 'attention_dropout': percentage of dropout for the attention layer
+        - 'attention_causal': adds a mask such that position i cannot attend to positions j > i.
+                              This prevents the flow of information from the future towards the past.
+
+    :param hparams: a dictionary containing some arguments that determine how the forecaster will be built
+    :return: a compiled tf.keras model
     """
 
-    s = hparams['base_layer_size']
+    s = hparams['layer_size']
     ratio = hparams['input_seq_length'] // hparams['output_seq_length']
+    name = 'enc{}{}_{}{}'.format(hparams['num_encoder_layers'], hparams['encoder_type'],
+                                 hparams['num_decoder_layers'], hparams['decoder_type'])
+
+    if hparams['encoder_type'] == 'uni':
+        make_enc_layer = lambda x: tf.keras.layers.LSTM(s, return_sequences=True)(x)
+    elif hparams['encoder_type'] == 'bi':
+        make_enc_layer = lambda x: tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(s // 2, return_sequences=True))(x)
+    else:
+        raise ValueError("encoder_type must be either 'uni' or 'bi'")
+
+    if hparams['decoder_type'] == 'uni':
+        make_dec_layer = lambda x: tf.keras.layers.LSTM(s, return_sequences=True)(x)
+    elif hparams['decoder_type'] == 'bi':
+        make_dec_layer = lambda x: tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(s // 2, return_sequences=True))(x)
+    else:
+        raise ValueError("decoder_type must be either 'uni' or 'bi'")
 
     inp = tf.keras.layers.Input(shape=(hparams['input_seq_length'], 1))
-    x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(s, return_sequences=True))(inp)
-    x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(s, return_sequences=True))(x)
-    x = tf.keras.layers.Reshape((hparams['output_seq_length'], ratio * s * 2))(x)
-    x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(1, return_sequences=True))(x)
-    out = tf.keras.layers.Dense(1)(x)
-    model = tf.keras.models.Model(inp, out, name='enc2bi_dec1bi')
+
+    enc = inp
+    for _ in range(hparams['num_encoder_layers']):
+        enc = make_enc_layer(enc)
+
+    dec = tf.keras.layers.Reshape((hparams['output_seq_length'], ratio * s))(enc)
+
+    if hparams['attention_type'] == 'mul':
+        dec = make_dec_layer(dec)
+        dec = tf.keras.layers.Attention(use_scale=hparams['attention_scale'],
+                                        dropout=hparams['attention_dropout'],
+                                        causal=hparams['attention_causal'])([dec, enc])
+        hparams['num_decoder_layers'] -= 1
+        name += '_mulattn'
+
+    elif hparams['attention_type'] == 'add':
+        dec = make_dec_layer(dec)
+        dec = tf.keras.layers.AdditiveAttention(use_scale=hparams['attention_scale'],
+                                                dropout=hparams['attention_dropout'],
+                                                causal=hparams['attention_causal'])([dec, enc])
+
+        hparams['num_decoder_layers'] -= 1
+        name += '_addattn'
+
+    elif hparams['attention_type'] == 'self':
+        dec = tf.keras.layers.Attention(use_scale=hparams['attention_scale'],
+                                        dropout=hparams['attention_dropout'],
+                                        causal=hparams['attention_causal'])([enc, enc])
+
+    for _ in range(hparams['num_decoder_layers']):
+        dec = make_dec_layer(dec)
+
+    out = tf.keras.layers.LSTM(1, return_sequences=True)(dec)
+
+    model = tf.keras.models.Model(inp, out, name=name)
+
     model.compile(loss='mae', optimizer='adam', metrics=['mae', 'mse'])
+
     return model
-
-
-def enc2bi_dec1uni(hparams: dict) -> tf.keras.models.Model:
-    """
-    Factory for creating a forecaster
-
-    :param hparams: a dictionary containing some hyperparameters that determine how the forecaster will be built
-    :return: a tf.keras model
-    """
-
-    s = hparams['base_layer_size']
-    ratio = hparams['input_seq_length'] // hparams['output_seq_length']
-
-    inp = tf.keras.layers.Input(shape=(hparams['input_seq_length'], 1))
-    x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(s, return_sequences=True))(inp)
-    x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(s, return_sequences=True))(x)
-    x = tf.keras.layers.Reshape((hparams['output_seq_length'], ratio * s * 2))(x)
-    x = tf.keras.layers.LSTM(1, return_sequences=True)(x)
-    model = tf.keras.models.Model(inp, x, name='enc2bi_dec1uni')
-    model.compile(loss='mae', optimizer='adam', metrics=['mae', 'mse'])
-    return model
-
-
-def enc2bi_dec1uni_attn(hparams: dict) -> tf.keras.models.Model:
-    """
-    Factory for creating an attention-based forecaster
-
-    :param hparams: a dictionary containing some hyperparameters that determine how the forecaster will be built
-    :return: a tf.keras model
-    """
-
-    s = hparams['base_layer_size']
-    ratio = hparams['input_seq_length'] // hparams['output_seq_length']
-
-    inp = tf.keras.layers.Input(shape=(hparams['input_seq_length'], 1))
-    enc = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(s, return_sequences=True))(inp)
-    enc = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(s, return_sequences=True))(enc)
-
-    res = tf.keras.layers.Reshape((hparams['output_seq_length'], ratio * s * 2))(enc)
-
-    dec = tf.keras.layers.LSTM(s * 2, return_sequences=True)(res)
-
-    att = tf.keras.layers.Attention(use_scale=hparams['attention_scale'],
-                                    dropout=hparams['attention_dropout'],
-                                    causal=hparams['attention_causal'])([dec, enc])
-
-    out = tf.keras.layers.LSTM(1, return_sequences=True)(att)
-
-    model = tf.keras.models.Model(inp, out, name='enc2bi_dec1uni_attn')
-    model.compile(loss='mae', optimizer='adam', metrics=['mae', 'mse'])
-    return model
-
-
-def enc2bi_dec1bi_attn(hparams: dict) -> tf.keras.models.Model:
-    """
-    Factory for creating an attention-based forecaster
-
-    :param hparams: a dictionary containing some hyperparameters that determine how the forecaster will be built
-    :return: a tf.keras model
-    """
-
-    s = hparams['base_layer_size']
-    ratio = hparams['input_seq_length'] // hparams['output_seq_length']
-
-    inp = tf.keras.layers.Input(shape=(hparams['input_seq_length'], 1))
-    enc = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(s, return_sequences=True))(inp)
-    enc = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(s, return_sequences=True))(enc)
-
-    res = tf.keras.layers.Reshape((hparams['output_seq_length'], ratio * s * 2))(enc)
-
-    dec = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(s, return_sequences=True))(res)
-
-    att = tf.keras.layers.Attention(use_scale=hparams['attention_scale'],
-                                    dropout=hparams['attention_dropout'],
-                                    causal=hparams['attention_causal'])([dec, enc])
-
-    out = tf.keras.layers.LSTM(1, return_sequences=True)(att)
-
-    model = tf.keras.models.Model(inp, out, name='enc2bi_dec1bi_attn')
-    model.compile(loss='mae', optimizer='adam', metrics=['mae', 'mse'])
-    return model
-
-
-model_mapping = {'enc2bi_dec1bi': enc2bi_dec1bi,
-                 'enc2bi_dec1uni': enc2bi_dec1uni,
-                 'enc2bi_dec1uni_attn': enc2bi_dec1uni_attn,
-                 'enc2bi_dec1bi_attn': enc2bi_dec1bi_attn}
 
 
 if __name__ == '__main__':
 
-    hp = {'base_layer_size': 128, 'input_seq_length': 18, 'output_seq_length': 6,
-          'attention_scale': False, 'attention_dropout': 0., 'attention_causal': False}
+    import argparse
 
-    for name, model_gen in model_mapping.items():
-        print(name)
-        model = model_gen(hp)
-        model.summary()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--layer-size', type=int, default=64, help='Size of the layers in the network.')
+    parser.add_argument('--attention-type', type=str, default='mul', help='Which attention type to use.')
+    parser.add_argument('--attention-dropout', type=float, default=0., help='Percentage of dropout for attention layer.')
+    parser.add_argument('--attention-causal', type=bool, default=False, help='Option to apply causal attention.')
+    parser.add_argument('--encoder-type', type=str, default='bi', help='What type of layers to use for encoder.')
+    parser.add_argument('--decoder-type', type=str, default='lstm', help='What type of layers to use for decoder.')
+    parser.add_argument('--encoder-layers', type=int, default=2, help='Number of layers to use for the encoder.')
+    parser.add_argument('--decoder-layers', type=int, default=1, help='Number of layers to use for the decoder.')
+
+    args = parser.parse_args()
+
+    params = args.__dict__
+    params.update({'input_seq_length': 18, 'output_seq_length': 6})
+
+    model = build_model(params)
+
+    model.summary()
